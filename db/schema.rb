888215bf3a099c0dfbecd2ab5e15256016d0bcb1
Nicoleta <60668003+nico-jsy@users.noreply.github.com>
2020-07-05 19:27:15 +0100

    added description to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_182047) do
+ActiveRecord::Schema.define(version: 2020_07_05_182033) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
 end
